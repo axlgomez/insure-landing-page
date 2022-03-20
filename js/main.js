9c314f1ea61b614690a6c88724212424d4788b16
Axel <axelgomez.js@gmail.com>
2022-03-20 11:59:21 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector("#menu");
+const myNav = document.querySelector("#myNav");
+
+menu.addEventListener("click", ()=>{
+    menu.classList.toggle("fa-xmark");
+    myNav.classList.toggle("active");
+});
